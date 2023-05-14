589e11339fd9daee603b80b2544656310cb61b5e
Marcus-Peterson <73422974+Marcus-Peterson@users.noreply.github.com>
2023-05-14 20:37:02 +0200

    Ladet till felhantering i while loop på rad 37 t.o.m rad 43 utifall någon lampa inte skulle fungera

--- Main.cpp
+++ Main.cpp
@@ -17,7 +17,7 @@ int main()
 {
   // Huvudfunktionen initierar UART, skapar och hanterar LED-lamporna
   USART2_Init();
-  
+
   // Skapar en lokal Led-instans (led2) av färgen BLUE och initialiserar tillståndet till ON
   Led led2(BLUE, ON);
   // Skapar en dynamiskt allokerad Led-instans (led3) av färgen YELLOW och initaliserar tillståndet till ON
@@ -29,22 +29,21 @@ int main()
     printf("Error: could not allocate memory for led3\n");
     return 1;
   }
-  //Hämtar led1:s tillstånd och sparar det i led1_state
+  // Hämtar led1:s tillstånd och sparar det i led1_state
   led1_state = led1.getState();
   // Sätter led1:s tillstånd till OFF
   led1.setState(OFF);
-  //Lade till felhantering utifall någon av lamporna inte skulle fungera
+  // Lade till felhantering utifall någon av lamporna inte skulle fungera
   while (1)
   {
     // Kontrollera om någon av LED-objekten är null
-    if (isLedNull(&led1) || isLedNull(&led2) || isLedNull(led3))
+    if(isLedNull(&led1) || isLedNull(&led2) || isLedNull(led3))
     {
       printf("Error: a LED object is null\n");
       return 1;
     }
-    
   }
-  
+
   // Frigör minnet allokerat till led3
   delete led3;
   return 0;
